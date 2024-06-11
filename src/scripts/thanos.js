cb800ca797ffdec47ddfdb58900fa82c1b66c1d1
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-06-11 23:56:31 +0200

    thanos added

--- src/scripts/thanos.js
+++ src/scripts/thanos.js
@@ -1,5 +1,21 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const thanos = async () => {};
+export const thanos = async () => {
+  try {
+    const contactsInArray = await fs.readFile(PATH_DB, 'utf8');
+    const parsedContactsArray = JSON.parse(contactsInArray);
+    const content = [];
+    for (let item of parsedContactsArray) {
+      if (Math.random() >= 0.5) {
+        content.push(item);
+      }
+    }
+
+    return fs.writeFile(PATH_DB, JSON.stringify(content));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 await thanos();
