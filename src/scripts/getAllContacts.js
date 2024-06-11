bb68e07d6709d03d5f0fa63bd13536b4517a6957
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-06-11 09:49:49 +0200

    get contacts added

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,5 +1,20 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const getAllContacts = async () => {};
+// У файлі src/scripts/getAllContacts.js опишіть функцію getAllContacts.
+//  Вона має повертати масив контактів із файлу src/db/db.json.
+//   Функція має коректно читати масив контактів з файлу.
+
+// 2a. Read array from file db.json
+// 2c. JSON.parse()
+
+export const getAllContacts = async () => {
+  try {
+    const allContacts = await fs.readFile(PATH_DB, 'utf8');
+    return JSON.parse(allContacts);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 console.log(await getAllContacts());
