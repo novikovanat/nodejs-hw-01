501ea3c8ca98290f2ac411c6c0b1c95ffdf292ad
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-06-11 09:59:59 +0200

    remove all contacts

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,14 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const removeAllContacts = async () => {};
+
+export const removeAllContacts = async () => {
+    try {
+    const content = [];
+    return fs.writeFile(PATH_DB, JSON.stringify(content));}
+ catch (error) {
+    console.log(error);
+  }
+};
 
 await removeAllContacts();
